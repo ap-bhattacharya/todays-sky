0ee93c81d5331b51e864844f6081df926c7cf602
Arunangshu Prasad Bhattacharya <109788065+ap-bhattacharya@users.noreply.github.com>
2025-05-01 16:14:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ particles_js = """<!DOCTYPE html>
     particlesJS("particles-js", {
       "particles": {
         "number": {
-          "value": 3000,
+          "value": 500,
           "density": {
             "enable": true,
             "value_area": 800
